2b5f52abb88e7e4951771731b9ed30e5470661ca
João Jamba <jambapedrojoao@gmail.com>
2024-10-10 16:55:32 +0100

    comparison operation

--- Day_3/Operators.md
+++ Day_3/Operators.md
@@ -22,3 +22,64 @@
     
     Assignment operators are used to assign values to variables. Let us take = as an example. Equal sign in mathematics shows thst values are equal, however in Python it means we are storing a value in a certain variable and we call it assignment or a assigning value to a variable. The table below the different types of python assignment operators.
 
+    Operator    Example     Same As
+    =           x=5         x=5
+    +=          x+=3        x=x+3
+    -=          x-=3        x=x-3
+    *=          x*=3        x=x*3
+    /=          x/=3        x=x/3
+    %=          x%=3        x=x%3
+    //=         x//=3       x=x//3
+    **=         x**=3       x=x**3
+    &=          x&=3        x=x&3
+    |=          z|=3        z=z|3
+    ^=          z^=3        z=z^3
+    >>=         z>>=3       z=z>>3
+    <<=         x<<=3       x=x<<3
+
+
+
+
+####    Arithmetic Operators:
+
+    Addition (+): a + b
+    Subtraction (-): a- b
+    Multiplication (*): a * b
+    Division (/): a / b
+    Modulus (%): a % b
+    Floor division (//): a // b
+    Exponentiation (**): a ** b
+
+
+    Operator    Name            Example
+    +           Additon         x + r
+    -           Subtraction     x - r
+    *           Multiplication  x * r 
+    /           Division        x / y
+    %           Modulus         r % w
+    **          Exponentiation  x ** y
+    //          Floor division  x // r
+
+
+
+See example in .py
+
+
+####    Comparison Operators:
+
+In programming we compare values, we use comparison operators to compare two values.
+We check if a value is greater or less or equal to other value. The following table shows
+Python comparison operators which was taken from w3shool.
+
+Operator        Name                                    Example
+__________________________________________________________________________
+==              Equal                                   x==y
+!=              Not equal                               x!=y
+>               Grater than                             x>y
+<               Less than                               x<y
+>=              Greater than or equal to                x>=y
+<=              Less than or equal to                   x<=y
+
+
+
+
--- Day_3/Operators.py
+++ Day_3/Operators.py
@@ -6,3 +6,160 @@
 print(True)
 print(False)
 
+
+
+'''
+    Example Integers
+    Arithmetic Operations in Python | Integers
+'''
+
+print('Addition:', 3 + 4)
+print('Subtraction:', 3 - 4)
+print('Multiplication:', 3 * 4)
+print('Division:', 3 / 4)
+print('Division without the remainder:', 3 // 4)
+print('Modules:', 3 % 4)
+print('Exponentiation:', 3 ** 4)
+print('Exponentiation:', 3 ^ 4)
+
+
+
+'''
+    Example Floats
+    Floating numbers
+'''
+
+print('Floating Point Number, PI', 3.14)
+print('Floating Point Number, gravity', 9.81)
+
+
+'''
+    Complex numbers
+'''
+
+print('Complex', 2 + 5j)
+print('Multiplying complex number: ', (2 + 3j) * (1 + 2j))
+
+
+
+'''
+    Let´s declare a variable and assign a number date type. I am going to use
+    single character variable but remember do not develop habit od declaring such types
+    of variables. Variable name should be all the time mnemonic.
+    
+    Example: Declaring the variable at the top first
+'''
+
+a = 5
+b = 5
+
+total = a + b
+diff = a - b
+product = a * b 
+division = b / a
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+
+'''
+    I should have used sum instead of total but sum is a built-in function -try to avoid
+    overriding built-in functions.
+    
+    If you do not label your print with some string, you never know where the result is coming from.
+'''
+
+print(total)
+print("a + b:   ", total)
+print("a - b:   ", diff)
+print("a * b:   ", product)
+print("a / b:   ", division)
+print("a % b:   ", remainder)
+print("a // b:  ", floor_division)
+print("a ** b:  ", exponential)
+
+
+'''
+    Example
+'''
+print("==   Addition, Subtraction, Multiplication, Division, Modules ==")
+
+number_one = 6
+number_two = 8
+
+# Arithmetic Operation
+total_ = number_one + number_two
+diff_ = number_one - number_two
+product_ = number_one * number_two
+div_ = number_one / number_two
+remainder_ = number_one % number_two
+
+
+# Printing Values With label
+print("Total: ", total_)
+print("Difference: ", diff_)
+print("Product: ", product_)
+print("Division: ", div_)
+print("Remainder: ", remainder_)
+
+
+'''
+    Let us start connecting the dots and start making us of what we already 
+    know to calculate (area, volume, density, weight, perimeter, distance, force).
+    
+    Example
+'''
+
+#   Calculating area of a circle
+radius = 20
+area_of_circle = 3.14 * radius ** 2
+print('Area of circle:  ', area_of_circle)
+
+
+#   Calculating area of a rectangle
+length = 10
+width = 30
+area_of_rectangle = length * width
+print('Area of rectangle: ', area_of_rectangle)
+
+
+#    Calculating a weight of an object 
+mass = 76
+gravity = 9.83
+weight = mass * gravity
+print(weight, ' N')
+
+
+# Calculate the density of a liquid
+mass_ = 75
+volume = 0.0234
+density = mass_ /  volume
+print(density, ' kg/m^3-')
+
+
+
+
+# Comparison Operators
+
+print("_____________________________________________________")
+print("____________________COMPARISON OPERATORS_____________")
+print(3>2)
+print(3>=2)
+print(3<2)
+print(3<=2)
+print(3==2)
+print(3!=2)
+print(len('mango') == len('avocado'))
+print(len('mango') != len('avocado'))
+print(len('mango') < len('avocado'))
+print(len('milk') != len('meat'))
+print(len('milk') == len('meat'))
+print(len('tomato') == len('potato'))
+print(len('python') > len('dragon'))
+
+print("_____________________________________________________")
+print("Comparing something gives either a True or False")
+print("_____________________________________________________")
+print('True == True', True == True)
+print('True == False', True == False)
+print('False == False', False == False)
